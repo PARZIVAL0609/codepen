c431643985139a6e34e789e2c0fd8b2e3820ab42
Dhruv Shah <dhruvshah6903@gmail.com>
2024-06-30 20:05:14 +0530

    Editor Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import React, { useState } from "react";
+import Editor from "./components/Editor";
+
 function App() {
-  return "Hello";
+  const [html, setHtml] = useState('');
+  const [css, setCss] = useState('');
+  const [js, setJs] = useState('');
+
+  console.log("Rendering App component");
+
+  return (
+    <>
+      <div className="pane top-pane">
+        <Editor displayName="HTML" language="xml" value={html} onChange={setHtml} />
+        <Editor displayName="CSS" language="css" value={css} onChange={setCss} />
+        <Editor displayName="JS" language="javascript" value={js} onChange={setJs} />
+      </div>
+      <div className="pane">
+        <iframe
+          title="output"
+          sandbox="allow-scripts"
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Editor.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+import "codemirror/mode/xml/xml";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/css/css";
+import { Controlled as ControlledEditor } from "react-codemirror2";
+
+const Editor = (props) => {
+  const { language, displayName, value, onChange } = props;
+
+  const handleChange = (editor, data, value) => {
+    onChange(value);
+  };
+
+
+  console.log(`Rendering ${displayName} editor`);
+  return (
+    <div className="editor-container">
+      <div className="editor-title">
+        {displayName}
+        <button>O/C</button>
+      </div>
+      <ControlledEditor
+        onBeforeChange={handleChange}
+        value={value}
+        className="code-mirror-wrapper"
+        options={{
+          lineWrapping: true,
+          mode: language,
+          lineNumbers: true,
+          theme: "material",
+        }}
+      />
+    </div>
+  );
+};
+
+export default Editor;
